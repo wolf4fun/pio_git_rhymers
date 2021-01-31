89d1e5bf1e1a5b0e4125bd28b3750af773df93d0
wolf4fun <wilczek4fun@gmail.com>
2021-01-31 18:23:49 +0100

    task 1.4 correction

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,37 +2,54 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    //renaming NUMBERS to numbers, small ommited correction 
-	private final int[] numbers = new int[12];
-    private int total = -1;
+    //! Correction based on https://gist.github.com/kowallus/af13f7e430fbf1a7c9a293b9e57e66bc
 
-    public int getTotal() {
-        return total;
-    }
+    private static int EMPTY_RHYMER_INDICATOR = -1;
+    private static final int IS_EMPTY = -1;
+    private static final int CAPACITY = 12;
 
+    private final int[] numbers = new int[CAPACITY];
+
+    /**
+     * @param in Adding element to numbers array
+     */
     public void countIn(int in) {
         if (!isFull())
-            numbers[++total] = in;
+            numbers[++EMPTY_RHYMER_INDICATOR] = in;
     }
 
+    /**
+     * @return boolean Checking if index of array has a default value
+     */
     public boolean callCheck() {
-        return total == -1;
+        return EMPTY_RHYMER_INDICATOR == IS_EMPTY;
     }
 
+    /**
+     * @return boolean Checking if array of numbers is full
+     */
     public boolean isFull() {
-        return total == 11;
+        return EMPTY_RHYMER_INDICATOR == CAPACITY - 1;
     }
 
+    /**
+     * @return int On succes returns the last element of array, DEFAULT_VALUE is
+     *         returned otherwise
+     */
     protected int peekaboo() {
         if (callCheck())
-            return -1;
-        return numbers[total];
+            return IS_EMPTY;
+        return numbers[EMPTY_RHYMER_INDICATOR];
     }
 
+    /**
+     * @return int On succes returns last element and decrements index of array
+     *         numbers, DEFAULT_VALUE is returned otherwise
+     */
     public int countOut() {
         if (callCheck())
-            return -1;
-        return numbers[total--];
+            return IS_EMPTY;
+        return numbers[EMPTY_RHYMER_INDICATOR--];
     }
 
 }
