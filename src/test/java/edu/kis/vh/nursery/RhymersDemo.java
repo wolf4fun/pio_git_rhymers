269ee3c6b0fed36031af94307158af3d1d32b5bd
wolf4fun <wilczek4fun@gmail.com>
2021-01-31 18:34:33 +0100

    task 1.5 refactoring

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -6,28 +6,31 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
-	public static void main(String[] args) {
-		Rhymersfactory factory = new DefaultRhymersFactory();
-
+	public static void testRhymers (Rhymersfactory factory)
+	{
 		DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-				factory.GetFIFORhymer(), factory.GetHanoiRhymer() };
-
+			factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
+	
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				rhymers[j].countIn(i);
-
+		
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < 15; i++)
 			rhymers[3].countIn(rn.nextInt(20));
-
+		
 		for (int i = 0; i < rhymers.length; i++) {
 			while (!rhymers[i].callCheck())
 				System.out.print(rhymers[i].countOut() + "  ");
 			System.out.println();
 		}
-
-		System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
-
+		
+		System.out.println("total rejected is "
+				+ ((HanoiRhymer) rhymers[3]).reportRejected());
 	}
-
+	public static void main(String[] args) {
+		Rhymersfactory factory = new DefaultRhymersFactory();
+		testRhymers(factory);
+	}
+	
 }
